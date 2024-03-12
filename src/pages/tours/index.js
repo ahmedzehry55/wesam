d435f09eedc21715e028caacfa4864775409a39b
Ahmed Zehry <ahmedalhlwany2111@gmail.com>
2024-03-12 09:05:30 +0200

    march16

--- src/pages/tours/index.js
+++ src/pages/tours/index.js
@@ -6,6 +6,7 @@ import { BreadcrumbList } from "@/components/pagesComponent/breadList/BreadList"
 import Hero from "../about/AboutComponet/Hero";
 import Image from "next/image";
 import Link from "next/link";
+import hero from "../../../public/images/tourHero.jpg";
 
 const breadcrumb1 = [
   {
