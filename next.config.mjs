412c8f07b449988292f27cfc82cd4bf20a5cd015
Ahmed Zehry <ahmedalhlwany2111@gmail.com>
2024-04-21 13:56:05 +0200

    march50

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,15 @@ const nextConfig = {
     });
 
     return config;
-  }
+  },
+  distDir: "_next",
+  generateBuildId: async () => {
+    if (process.env.BUILD_ID) {
+      return process.env.BUILD_ID;
+    } else {
+      return `${new Date().getTime()}`;
+    }
+  },
 };
 
 export default nextConfig;
